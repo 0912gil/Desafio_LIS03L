dd3384fb6152103000faf0b1982a8ab3a6a19d01
Jocelyn.gil <anasulma1230@gmail.com>
2022-02-17 20:48:04 -0600

    listado

--- index.php
+++ index.php
@@ -1,41 +1,74 @@
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" user-scalable="no">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <style>
-        a {
-            text-decoration: none;
-            font-weight: 500;
-            color: black;
-        }
-    </style>
-    <title>Productos</title>
+    <meta charset="utf-8">
+    <title>Ingreso de Productos Existentes</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <!-- JavaScript -->
+<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+
+<!-- CSS -->
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css"/>
 </head>
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 px-2 py-2 my-2 offset-md-1">
-                <div class="card">
-                    <div class="card-header">
-                        <h2 class="text-center">Listado de opciones</h2>
-                    </div>
-                    <div class="card-body">
-                        <ul class="list-group">
-                            <li class="list-group-item"><a href="admin/index.php">Administrador</a></li>
-                            <li class="list-group-item"><a href="cliente/usuario.php">Usuario</a></li>
-                            
-                        </ul>
-                    </div>
-                    <div class="card-footer text-muted text-center">
-                        Ana Jocelyn Gil Torres - GT182673 
-                        Ricardo Arturo De Paz Núñez - DN192246
-                    </div>
-                </div>
-            </div>
+<div class="container">
+    <h1 class="page-header text-center">Ingresar Productos Existentes</h1>
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+            <a href="#addnew" class="btn btn-primary" data-toggle="modal"><span class="glyphicon glyphicon-plus"></span> Agregar Producto</a>
+            
+            <table class="table table-bordered table-striped" style="margin-top:20px;">
+                <thead>
+                    <th>Código</th>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Imagen</th>
+                    <th>Categoría</th>
+                    <th>Precio</th>
+                    <th>Existencia</th>
+                </thead>
+                <tbody>
+                    <?php
+                    $productos=simplexml_load_file('productos.xml');
+                    foreach ($productos ->producto as $row) {
+                                        
+                        ?>
+                        <tr>
+                            <td><?=$row->codigo?></td>
+                            <td><?=$row->nombre?></td>
+                            <td><?=$row->descripcion?></td>
+                            <td><?=$row->imagen?></td>
+                            <td><?=$row->categoria?></td>
+                            <td><?=$row->precio?></td>
+                            <td><?=$row->existencia?></td>
+                            <td>
+                                <a href="#" class="btn btn-success">Editar</a>
+                                <a href="#delete_<?=$row->codigo?>" data-toggle="modal" class="btn btn-danger">Eliminar</a>
+                            </td>
+                        </tr>
+                        <?php include('borrar_producto.php');?>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div>
-</body>
+</div>
+<?php include('nuevo_producto.php');?>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<?php
+if(isset($_GET['exito'])){
 
+
+?>
+<script>
+    alertify.success('Producto insertado exitosamente');
+</script>
+<?php
+}
+?>
+</body>
 </html>
