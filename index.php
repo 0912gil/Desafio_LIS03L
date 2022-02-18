0d0069e7bd04b5e21d8ff1f2cd6c74db307e781c
Jocelyn.gil <anasulma1230@gmail.com>
2022-02-17 20:40:18 -0600

    listado

---
+++ index.php
@@ -0,0 +1,41 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" user-scalable="no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        a {
+            text-decoration: none;
+            font-weight: 500;
+            color: black;
+        }
+    </style>
+    <title>Productos</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 px-2 py-2 my-2 offset-md-1">
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="text-center">Listado de opciones</h2>
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="admin/index.php">Administrador</a></li>
+                            <li class="list-group-item"><a href="cliente/usuario.php">Usuario</a></li>
+                            
+                        </ul>
+                    </div>
+                    <div class="card-footer text-muted text-center">
+                        Ana Jocelyn Gil Torres - GT182673 
+                        Ricardo Arturo De Paz Núñez - DN192246
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
